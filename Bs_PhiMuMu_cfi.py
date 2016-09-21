450ed5e965d34b6c34db33ff45e9e1910c76595b
oozcelik <ozlem.ozcelik@cern.ch>
2016-09-21 17:34:45 +0300

    Create Bs_PhiMuMu_cfi.py

---
+++ Bs_PhiMuMu_cfi.py
@@ -0,0 +1,81 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.0),
+                         ##crossSection = cms.untracked.double(54000000000), # Given by PYTHIA after running
+                         ##filterEfficiency = cms.untracked.double(0.004), # Given by PYTHIA after running
+                         maxEventsToPrint = cms.untracked.int32(0),
+
+
+                         ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
+            user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/Bs_phimumu_KK.dec'),
+            list_forced_decays = cms.vstring('MyB_s0','Myanti-B_s0'),
+            operates_on_particles = cms.vint32()
+            ),
+        parameterSets = cms.vstring('EvtGen130')
+        ),
+
+
+                         PythiaParameters = cms.PSet(pythia8CommonSettingsBlock,
+                                                     pythia8CUEP8M1SettingsBlock,
+                                                     ## check this (need extra parameters?)
+                                                     processParameters = cms.vstring("SoftQCD:nonDiffractive = on"),
+                                                     parameterSets = cms.vstring('pythia8CommonSettings',
+                                                                                 'pythia8CUEP8M1Settings',
+                                                                                 'processParameters',
+                                                                                 )
+                                                     )
+                         )
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: Configuration/Generator/python/PYTHIA8_Bs2JpsiPhi_EtaPtFilter_CUEP8M1_13TeV_cff.py  $'),
+    annotation = cms.untracked.string('Summer16: Pythia8+EvtGen130 generation of Bs --> Jpsi(-> mu+ mu-) Phi(K+ K-) , 13TeV, Tune CUETP8M1')
+    )
+
+###########
+# Filters #
+###########
+
+bfilter = cms.EDFilter(
+    "PythiaFilter", 
+    MaxEta = cms.untracked.double(9999.),
+    MinEta = cms.untracked.double(-9999.),
+    ParticleID = cms.untracked.int32(531)  ## Bs
+    )
+
+decayfilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(3),
+    ParticleID      = cms.untracked.int32(531),
+    DaughterIDs     = cms.untracked.vint32(-13, 13, 333),  ## mu+, mu-, phi(1020)
+    MinPt           = cms.untracked.vdouble(2.5, 2.5, -1.),
+    MinEta          = cms.untracked.vdouble(-2.5, -2.5, -9999.),
+    MaxEta          = cms.untracked.vdouble( 2.5,  2.5,  9999.)
+    )
+
+phifilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1), 
+    NumberDaughters = cms.untracked.int32(2), 
+    MotherID        = cms.untracked.int32(531),  ## Bs
+    ParticleID      = cms.untracked.int32(333),  ## phi(1020)
+    DaughterIDs     = cms.untracked.vint32(321, -321), ## K+, K-
+    MinPt           = cms.untracked.vdouble(0.4, 0.4), 
+    MinEta          = cms.untracked.vdouble(-2.5, -2.5), 
+    MaxEta          = cms.untracked.vdouble( 2.5,  2.5)
+    )
+
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*decayfilter*phifilter)
