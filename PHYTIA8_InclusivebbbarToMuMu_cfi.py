820b9cc4146f956b1131a8e72465c4c314cbfd7d
oozcelik <ozlem.ozcelik@cern.ch>
2017-01-26 20:48:47 +0200

    Update PHYTIA8_InclusivebbbarToMuMu_cfi.py

--- PHYTIA8_InclusivebbbarToMuMu_cfi.py
+++ PHYTIA8_InclusivebbbarToMuMu_cfi.py
@@ -13,7 +13,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
           ExternalDecays = cms.PSet(
             EvtGen130 = cms.untracked.PSet(
             operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
-            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010_NOLONGLIFE.DEC'),
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
              ),
             parameterSets = cms.vstring('EvtGen130')
@@ -23,7 +23,6 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
         pythia8CUEP8M1SettingsBlock,
         processParameters = cms.vstring(
             'SoftQCD:nonDiffractive = on',            
-            'ParticleDecays:mixB = on', # B-mixing allowed
          ),  
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CUEP8M1Settings',
@@ -39,6 +38,7 @@ configurationMetadata = cms.untracked.PSet(
     name = cms.untracked.string('$Source: Configuration/Generator/python/PHYTIA8_bbbarToMuMu_1.cff_py $'),
     annotation = cms.untracked.string('Spring 2015: Pythia8 generation enriched with b-bbar -> mumu, 13TeV, Tune CUEP8M1')
     )
+
 bbbarfilter = cms.EDFilter("MCParticlePairFilter",
     Status = cms.untracked.vint32(1, 1),
     MinPt = cms.untracked.vdouble(0., 0.),
@@ -49,13 +49,13 @@ bbbarfilter = cms.EDFilter("MCParticlePairFilter",
 )
 
 
-mumugenfilter = cms.EDFilter("MCParticlePairFilter",
-    Status = cms.untracked.vint32(1, 1),
-    MinPt = cms.untracked.vdouble(3., 3.),
-    MaxEta = cms.untracked.vdouble(3., 3.),
-    MinEta = cms.untracked.vdouble(-3., -3.),
-    ParticleID1 = cms.untracked.vint32(13),
-    ParticleID2 = cms.untracked.vint32(-13)
-)
-
+mumugenfilter  = cms.EDFilter("MCMultiParticleFilter",
+    src = cms.InputTag('generator'),   
+    Status = cms.vint32(1),
+    ParticleID = cms.vint32(13),
+    PtMin = cms.vdouble(3.),
+    NumRequired = cms.int32(2),
+    EtaMax = cms.vdouble(3.),
+    AcceptMore = cms.bool(True)
+            )
 ProductionFilterSequence = cms.Sequence(generator*bbbarfilter*mumugenfilter)
