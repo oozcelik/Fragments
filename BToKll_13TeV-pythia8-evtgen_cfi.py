968406b4810aa5d06fc9ac15f2b506db5838528a
Ozlem Ozcelik <ozlem.ozcelik@cern.ch>
2018-03-05 21:13:58 +0300

    Update BToKll_13TeV-pythia8-evtgen_cfi.py

--- BToKll_13TeV-pythia8-evtgen_cfi.py
+++ BToKll_13TeV-pythia8-evtgen_cfi.py
@@ -1,51 +1,47 @@
 import FWCore.ParameterSet.Config as cms
-
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-                         pythiaHepMCVerbosity = cms.untracked.bool(False),
-                         maxEventsToPrint = cms.untracked.int32(0),
-                         pythiaPylistVerbosity = cms.untracked.int32(0),
-                         filterEfficiency = cms.untracked.double(1.38e-3),
-                         crossSection = cms.untracked.double(540000000.),
-                         comEnergy = cms.double(13000.0),
-
-                         ExternalDecays = cms.PSet(
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(13000.0),
+    ExternalDecays = cms.PSet(
         EvtGen130 = cms.untracked.PSet(
             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
-            user_decay_embedded= cms.vstring("""
-            Decay B+
-            0.000000550 K+      e+      e-     PHOTOS BTOSLLBALL; #[Reconstructed PDG2011];
-            0.000000520 K+      mu+     mu-    PHOTOS BTOSLLBALL; #[Reconstructed PDG2011];
-            Enddecay   
-            CDecay B-
-            """
-            list_forced_decays = cms.vstring(),
-            ),
-        operates_on_particles = cms.vint32('521'),
-        parameterSets = cms.vstring('EvtGen130')
+            list_forced_decays = cms.vstring(),        
+            operates_on_particles = cms.vint32(521),    # we care just about our signal particles
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded= cms.vstring(
+"""
+Decay myB+
+0.000000550 K+      e+      e-     PHOTOS BTOSLLBALL;
+0.000000520 K+      mu+     mu-    PHOTOS BTOSLLBALL;
+Enddecay
+CDecay B-
+End
+"""
+            )
         ),
-                         
-                         PythiaParameters = cms.PSet(pythia8CommonSettingsBlock,
-                                                     pythia8CP5SettingsBlock,
-                                                     processParameters = cms.vstring('SoftQCD:nonDiffractive = on'                                                                                   
-                                                                                    ),
-                                                     parameterSets = cms.vstring('pythia8CommonSettings',
-                                                                                 'pythia8CP5Settings',
-                                                                                 'processParameters',
-                                                                                 )
-                                                     )
-                         )
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring('SoftQCD:nonDiffractive = on'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
 
-###########
-# Filters #
-###########
-# Filter only pp events which produce a B+:
-                         
+###### Filters ##########
 bufilter = cms.EDFilter(
-    "PythiaFilter", 
+    "PythiaFilter",
     MaxEta = cms.untracked.double(9999.),
     MinEta = cms.untracked.double(-9999.),
     ParticleID = cms.untracked.int32(521) ## Bu
@@ -53,20 +49,19 @@ bufilter = cms.EDFilter(
 
 decayfilterpositiveleg = cms.EDFilter(
     "PythiaDauVFilter",
-    verbose         = cms.untracked.int32(1), 
-    NumberDaughters = cms.untracked.int32(2), 
-    ParticleID      = cms.untracked.int32(521),  ## Bu  
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(2),
+    ParticleID      = cms.untracked.int32(521),  ## Bu
     DaughterIDs     = cms.untracked.vint32(321, 13, 11), ## K+ and either mu- or e-
-    MinPt           = cms.untracked.vdouble(-1., -1., -1.),  
-    MinEta          = cms.untracked.vdouble(-9999., -9999., -9999.), 
+    MinPt           = cms.untracked.vdouble(-1., -1., -1.),
+    MinEta          = cms.untracked.vdouble(-9999., -9999., -9999.),
     MaxEta          = cms.untracked.vdouble( 9999.,  9999.,  9999.)
     )
-
-process.mufilter = cms.EDFilter("PythiaFilter",  # bachelor muon with kinematic cuts.
+mufilter = cms.EDFilter("PythiaFilter",  # bachelor muon with kinematic cuts.
     MaxEta = cms.untracked.double(2.5),
     MinEta = cms.untracked.double(-2.5),
     MinPt = cms.untracked.double(7.),
     ParticleID = cms.untracked.int32(13),
 )
-                 
- ProductionFilterSequence = cms.Sequence(generator*bufilter*decayfilterpositiveleg*muFilter)                         
+
+ProductionFilterSequence = cms.Sequence(generator*bufilter*decayfilterpositiveleg*mufilter)
