e8711dc4e9ba3491456c1d3cb1a8999ba9d84fb8
Ozlem Ozcelik <ozlem.ozcelik@cern.ch>
2018-03-14 10:38:00 +0300

    second commit

---
+++ BToKMuMu_13TeV-pythia8-evtgen_cfi.py
@@ -0,0 +1,68 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(13000.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            list_forced_decays = cms.vstring('MyB+','MyB-'),        
+            operates_on_particles = cms.vint32(),    
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded= cms.vstring(
+"""
+Alias      MyB+        B+
+Alias      MyB-        B-
+ChargeConj MyB-        MyB+
+#
+Decay MyB+
+1.000  K+      mu+     mu-    PHOTOS BTOSLLBALL;
+Enddecay
+CDecay MyB-
+#
+End
+"""
+            )
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring('SoftQCD:nonDiffractive = on'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+###### Filters ##########
+decayfilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(3),
+    ParticleID      = cms.untracked.int32(521),  ## Bu
+    DaughterIDs     = cms.untracked.vint32(321, 13, -13), ## K+ mu- mu+
+    MinPt           = cms.untracked.vdouble(-1., -1., -1.),
+    MinEta          = cms.untracked.vdouble(-9999., -9999., -9999.),
+    MaxEta          = cms.untracked.vdouble( 9999.,  9999.,  9999.)
+)
+    
+multiFilter = cms.EDFilter("MCMultiParticleFilter",
+            src = cms.untracked.InputTag("generator", "unsmeared"),   
+            Status = cms.vint32(1),
+            ParticleID = cms.vint32(13),
+            PtMin = cms.vdouble(-1.),
+            NumRequired = cms.int32(3),
+            EtaMax = cms.vdouble(-1.),
+            AcceptMore = cms.bool(True)
+            )
+
+ProductionFilterSequence = cms.Sequence(generator*decayfilter*mufilter)
